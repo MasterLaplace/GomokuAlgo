33f433db635432fe0091fc0d1b4b0f09b62e7c4f
Master_Laplace <guillaume.papineau@epitech.eu>
2023-11-14 08:08:16 +0100

    refactor(ai): evaluate and minimax functions for better performance

--- src/ai/brain.py
+++ src/ai/brain.py
@@ -5,7 +5,6 @@
 # Author: MasterLaplace
 # Created on: 2023-11-7
 
-from random import randint
 from enum import Enum
 from game.game import Game
 
@@ -51,103 +50,130 @@ class Brain:
             bool: _description_ True if there are moves left, False otherwise
         """
 
-        for i in range(0, size[0]) :
-            for j in range(0, size[1]) :
+        for i in range(size[0]) :
+            for j in range(size[1]) :
                 if board[i][j] == Game.CaseSate.EMPTY:
                     return True
         return False
 
     @staticmethod
-    def evaluate(board: list[list[str]], size: tuple[int, int]) -> Game.CaseSate:
-        for i in range(0, size[0]):
-            for j in range(0, size[1]):
+    def evaluate(board: list[list[str]], size: tuple[int, int]) -> int:
+        """_summary_ Evaluate the board and return the score
+
+        Args:
+            board (list[list[str]]): _description_ The board of the game
+            size (tuple[int, int]): _description_ The size of the board
+
+        Returns:
+            int: _description_ The score of the board (10 if PLAYER1 wins, -10 if PLAYER2 wins, 0 otherwise)
+        """
+        for i in range(size[0]):
+            for j in range(size[1]):
+                if board[i][j] == Game.CaseSate.EMPTY:
+                    continue
                 # check horizontal
                 if j + 4 < size[1]:
                     if board[i][j] == board[i][j + 1] == board[i][j + 2] == board[i][j + 3] == board[i][j + 4]:
-                        return Game.CaseSate.PLAYER1 if board[i][j] == Game.CaseSate.PLAYER1 else Game.CaseSate.PLAYER2
+                        return 10 if board[i][j] == Game.CaseSate.PLAYER1 else -10
                 # check vertical
                 if i + 4 < size[0]:
                     if board[i][j] == board[i + 1][j] == board[i + 2][j] == board[i + 3][j] == board[i + 4][j]:
-                        return Game.CaseSate.PLAYER1 if board[i][j] == Game.CaseSate.PLAYER1 else Game.CaseSate.PLAYER2
+                        return 10 if board[i][j] == Game.CaseSate.PLAYER1 else -10
                 # check diagonal
                 if i + 4 < size[0] and j + 4 < size[1]:
                     if board[i][j] == board[i + 1][j + 1] == board[i + 2][j + 2] == board[i + 3][j + 3] == board[i + 4][j + 4]:
-                        return Game.CaseSate.PLAYER1 if board[i][j] == Game.CaseSate.PLAYER1 else Game.CaseSate.PLAYER2
-        return Game.CaseSate.EMPTY
+                        return 10 if board[i][j] == Game.CaseSate.PLAYER1 else -10
+                # check anti-diagonal
+                if i + 4 < size[0] and j - 4 >= 0:
+                    if board[i][j] == board[i + 1][j - 1] == board[i + 2][j - 2] == board[i + 3][j - 3] == board[i + 4][j - 4]:
+                        return 10 if board[i][j] == Game.CaseSate.PLAYER1 else -10
+        return 0
 
     @staticmethod
-    def minimax(board: list[list[str]], size: tuple[int, int], depth: int, isMax: bool, opponent: Game.CaseSate) -> int:
+    def minimax(board: list[list[str]], size: tuple[int, int], depth: int, isMax: bool, alpha: int, beta: int) -> int:
+        """_summary_ Minimax algorithm to find the best solution
+
+        Args:
+            board (list[list[str]]): _description_ The board of the game
+            size (tuple[int, int]): _description_ The size of the board
+            depth (int): _description_ The depth of the algorithm (number of moves to look ahead)
+            isMax (bool): _description_ True if it's the turn of the player 1, False otherwise
+            alpha (int): _description_ The alpha value for the alpha-beta pruning
+            beta (int): _description_ The beta value for the alpha-beta pruning
+
+        Returns:
+            int: _description_ The score of the board (10 if PLAYER1 wins, -10 if PLAYER2 wins, best score otherwise)
+        """
         score = Brain.evaluate(board, size)
 
-        if score == Game.CaseSate.PLAYER1:
-            return 10
-        if score == Game.CaseSate.PLAYER2:
-            return -10
-        if not Brain.isMovesLeft(board, size):
-            return 0
-
-        opponent_positions = Brain.getOpponentPositions(board, size, opponent)
+        if depth == 0 or score != 0:
+            return score
 
         if isMax:
             best = -1000
-            for i in range(0, size[0]):
-                for j in range(0, size[1]):
+
+            for i in range(size[0]):
+                for j in range(size[1]):
                     if board[i][j] == Game.CaseSate.EMPTY:
-                        board[i][j] = Game.CaseSate.PLAYER2
-                        moveVal = Brain.minimax(board, size, depth + 1, not isMax, opponent)
-
-                        for pos in opponent_positions:
-                            if pos[0] == i or pos[1] == j or abs(pos[0] - i) == abs(pos[1] - j):
-                                moveVal += 1
-
+                        board[i][j] = Game.CaseSate.PLAYER1
+                        best = max(best, Brain.minimax(board, size, depth - 1, not isMax, alpha, beta))
                         board[i][j] = Game.CaseSate.EMPTY
-                        best = max(best, moveVal)
+
+                        alpha = max(alpha, best)
+                        if beta <= alpha:
+                            break  # Beta pruning
+
             return best
         else:
             best = 1000
-            for i in range(0, size[0]):
-                for j in range(0, size[1]):
+
+            for i in range(size[0]):
+                for j in range(size[1]):
                     if board[i][j] == Game.CaseSate.EMPTY:
                         board[i][j] = Game.CaseSate.PLAYER2
-                        moveVal = Brain.minimax(board, size, depth + 1, not isMax, opponent)
-
-                        for pos in opponent_positions:
-                            if pos[0] == i or pos[1] == j or abs(pos[0] - i) == abs(pos[1] - j):
-                                moveVal -= 1
-
+                        best = min(best, Brain.minimax(board, size, depth - 1, not isMax, alpha, beta))
                         board[i][j] = Game.CaseSate.EMPTY
-                        best = min(best, moveVal)
+
+                        beta = min(beta, best)
+                        if beta <= alpha:
+                            break  # Alpha pruning
+
             return best
 
     def findBestSolution(self, board: list[list[Game.CaseSate]], size: tuple[int, int]) -> tuple[int, int]:
-        bestVal = -1000
-        bestMove = (-1, -1)
+        """_summary_ Find the best solution for the next turn
 
-        opponent = Game.CaseSate.PLAYER2
+        __description__ Find the best solution for the next turn by using the minimax algorithm
+
+        __param__ board: The board of the game (list[list[PLAYER1, PLAYER2, EMPTY]])
+        __param__ size: The size of the board (tuple(x, y))
+
+        __return__ tuple[int, int]: The best solution for the next turn
+        """
+        bestVal: int = -1000
+        bestMove: tuple[int, int] = (-1, -1)
+        depth_limit: int = 4
 
         for i in range(size[0]):
             for j in range(size[1]):
                 if board[i][j] == Game.CaseSate.EMPTY:
                     board[i][j] = Game.CaseSate.PLAYER1
-                    moveVal = Brain.minimax(board, size, 0, False, opponent)
+                    moveVal = Brain.minimax(board, size, depth_limit, False, -float('inf'), float('inf'))
                     board[i][j] = Game.CaseSate.EMPTY
 
                     if moveVal > bestVal:
                         bestMove = (i, j)
                         bestVal = moveVal
 
+        if (bestMove[0] == -1 and bestMove[1] == -1):
+            for i in range(size[0]):
+                for j in range(size[1]):
+                    if board[i][j] == Game.CaseSate.EMPTY:
+                        bestMove = (i, j)
+                        break
         print(f"{bestMove[0]},{bestMove[1]}")
         return bestMove
 
-    @staticmethod
-    def getOpponentPositions(board: list[list[Game.CaseSate]], size: tuple[int, int], opponent: Game.CaseSate) -> list[tuple[int, int]]:
-        positions = []
-        for i in range(size[0]):
-            for j in range(size[1]):
-                if board[i][j] == opponent:
-                    positions.append((i, j))
-        return positions
-    
     def sendCommand(self):
         # TODO: Send command to the protocol
         pass
