f40f6193744e85af0218b4caea153a3f822403f2
Mathys Thévenot <mathysthevenot@macbook-pro-de-mathys.home>
2023-11-23 22:54:45 +0100

    feat(src/ai/minmax.c) : working algorithm

--- src/ai/minmax.c
+++ src/ai/minmax.c
@@ -1,137 +1,167 @@
-/*
-** EPITECH PROJECT, 2023
-** B-AIA-500-REN-5-1-gomoku-enzo.monnier
-** File description:
-** minmax
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
-#include <unistd.h>
-#include <stdint.h>
+#include <limits.h>
 
 typedef unsigned char u_my_type;
 
-#define MAX_DEPTH 2
+#define MAX_DEPTH 1
 #define EMPTY (u_my_type)'0'
 #define PLAYER (u_my_type)'1'
 #define OPPONENT (u_my_type)'2'
-
-#define HEIGHT board->size[0]
-#define WIDTH board->size[1]
 
 typedef struct {
     unsigned size[2];
     u_my_type *board;
 } board_t;
 
-// bool type and values
-#define bool _Bool
-#define false 0
-#define true 1
-
-// min, max functions
-#define typeof __typeof__
-#define max(a,b) (a > b ? a : b)
-#define min(a,b) (a < b ? a : b)
-
+#define HEIGHT board->size[0]
+#define WIDTH board->size[1]
 #define AT(_board) _board->board[i * WIDTH + j]
-#define AT_H(_n) board->board[i * WIDTH + j + _n]
-#define AT_V(_n) board->board[(i + _n) * WIDTH + j]
-#define AT_D(_n) board->board[(i + _n) * WIDTH + j + _n]
-#define AT_A(_n) board->board[(i + _n) * WIDTH + j - _n]
-#define RET (10 << (AT(board) == PLAYER)) - (10 << (AT(board) == OPPONENT))
+#define AT_H(_offset) board->board[i * WIDTH + j + _offset]
+#define AT_V(_offset) board->board[(i + _offset) * WIDTH + j]
+#define AT_D(_offset) board->board[(i + _offset) * WIDTH + j + _offset]
+#define AT_A(_offset) board->board[(i + _offset) * WIDTH + j - _offset]
 
-static bool isMovesLeft(board_t *board)
-{
-    for (unsigned i = 0; i < HEIGHT; i++)
-        for (unsigned j = 0; j < WIDTH; j++)
-            if (AT(board) == EMPTY)
-                return true;
+// Define min, max functions
+#define min(a,b) ((a) < (b) ? (a) : (b))
+#define max(a,b) ((a) > (b) ? (a) : (b))
+
+// Count consecutive pieces
+int count_consecutive(board_t *board, unsigned i, unsigned j, u_my_type player, int di, int dj) {
+    int count = 0;
+    while (i >= 0 && i < HEIGHT && j >= 0 && j < WIDTH && AT(board) == player) {
+        count++;
+        i += di;
+        j += dj;
+    }
+    return count;
+}
+
+// Check for a near-win condition for a player
+bool is_near_win(board_t *board, u_my_type player) {
+    for (unsigned i = 0; i < HEIGHT; i++) {
+        for (unsigned j = 0; j < WIDTH; j++) {
+            if (AT(board) == EMPTY) {
+                // Check for horizontal win
+                if (count_consecutive(board, i, j, player, 0, 1) >= 3) {
+                    return true;
+                }
+
+                // Check for vertical win
+                if (count_consecutive(board, i, j, player, 1, 0) >= 3) {
+                    return true;
+                }
+
+                // Check for diagonal win
+                if (count_consecutive(board, i, j, player, 1, 1) >= 3) {
+                    return true;
+                }
+
+                // Check for anti-diagonal win
+                if (count_consecutive(board, i, j, player, 1, -1) >= 3) {
+                    return true;
+                }
+            }
+        }
+    }
     return false;
 }
 
-static int evaluate(board_t *board) {
-    for (int i = 0; i < (int)HEIGHT; i++) {
-        for (int j = 0; j < (int)WIDTH; j++) {
+// Evaluation function
+// Evaluation function
+int evaluate(board_t *board) {
+    int score = 0;
+    const int WIN_SCORE = 1000000;
+    const int FOUR_IN_A_ROW_SCORE = 500000;
+    const int THREE_IN_A_ROW_SCORE = 10000;
+    const int TWO_IN_A_ROW_SCORE = 100;
+
+    for (unsigned i = 0; i < HEIGHT; i++) {
+        for (unsigned j = 0; j < WIDTH; j++) {
             if (AT(board) == EMPTY)
                 continue;
 
-            if (j + 3 < (int)WIDTH && AT(board) == AT_H(1) && AT(board) == AT_H(2) && AT(board) == AT_H(3))
-                if (AT(board) == OPPONENT)
-                    return -10;
+            // Check for immediate win for us and immediate loss for opponent
+            if ((AT(board) == PLAYER && is_near_win(board, PLAYER)) ||
+                (AT(board) == OPPONENT && is_near_win(board, OPPONENT))) {
+                return AT(board) == PLAYER ? WIN_SCORE : -WIN_SCORE;
+            }
 
-            if (i + 3 < (int)HEIGHT && AT(board) == AT_V(1) && AT(board) == AT_V(2) && AT(board) == AT_V(3))
-                if (AT(board) == OPPONENT)
-                    return -10;
+            // Check for four in a row
+            if ((j + 3 < WIDTH && AT(board) == AT_H(1) && AT(board) == AT_H(2) && AT(board) == AT_H(3)) ||
+                (i + 3 < HEIGHT && AT(board) == AT_V(1) && AT(board) == AT_V(2) && AT(board) == AT_V(3)) ||
+                (i + 3 < HEIGHT && j + 3 < WIDTH && AT(board) == AT_D(1) && AT(board) == AT_D(2) && AT(board) == AT_D(3)) ||
+                (i + 3 < HEIGHT && j - 3 >= 0 && AT(board) == AT_A(1) && AT(board) == AT_A(2) && AT(board) == AT_A(3))) {
+                score += (AT(board) == PLAYER ? FOUR_IN_A_ROW_SCORE : -FOUR_IN_A_ROW_SCORE);
+            }
 
-            if (i + 3 < (int)HEIGHT && j + 3 < (int)WIDTH && AT(board) == AT_D(1) && AT(board) == AT_D(2) && AT(board) == AT_D(3))
-                if (AT(board) == OPPONENT)
-                    return -10;
+            // Check for three in a row
+            if ((j + 2 < WIDTH && AT(board) == AT_H(1) && AT(board) == AT_H(2)) ||
+                (i + 2 < HEIGHT && AT(board) == AT_V(1) && AT(board) == AT_V(2)) ||
+                (i + 2 < HEIGHT && j + 2 < WIDTH && AT(board) == AT_D(1) && AT(board) == AT_D(2)) ||
+                (i + 2 < HEIGHT && j - 2 >= 0 && AT(board) == AT_A(1) && AT(board) == AT_A(2))) {
+                score += (AT(board) == PLAYER ? THREE_IN_A_ROW_SCORE : -THREE_IN_A_ROW_SCORE);
+            }
 
-            if (i + 3 < (int)HEIGHT && j - 3 >= 0 && AT(board) == AT_A(1) && AT(board) == AT_A(2) && AT(board) == AT_A(3))
-                if (AT(board) == OPPONENT)
-                    return -10;
-
-            if (j + 4 < (int)WIDTH && AT(board) == AT_H(1) && AT(board) == AT_H(2) && AT(board) == AT_H(3) && AT(board) == AT_H(4))
-                if (AT(board) == PLAYER)
-                    return 10;
-
-            if (i + 4 < (int)HEIGHT && AT(board) == AT_V(1) && AT(board) == AT_V(2) && AT(board) == AT_V(3) && AT(board) == AT_V(4))
-                if (AT(board) == PLAYER)
-                    return 10;
-
-            if (i + 4 < (int)HEIGHT && j + 4 < (int)WIDTH && AT(board) == AT_D(1) && AT(board) == AT_D(2) && AT(board) == AT_D(3) && AT(board) == AT_D(4))
-                if (AT(board) == PLAYER)
-                    return 10;
-
-            if (i + 4 < (int)HEIGHT && j - 4 >= 0 && AT(board) == AT_A(1) && AT(board) == AT_A(2) && AT(board) == AT_A(3) && AT(board) == AT_A(4))
-                if (AT(board) == PLAYER)
-                    return 10;
+            // Check for two in a row
+            if ((j + 1 < WIDTH && AT(board) == AT_H(1)) ||
+                (i + 1 < HEIGHT && AT(board) == AT_V(1)) ||
+                (i + 1 < HEIGHT && j + 1 < WIDTH && AT(board) == AT_D(1)) ||
+                (i + 1 < HEIGHT && j - 1 >= 0 && AT(board) == AT_A(1))) {
+                score += (AT(board) == PLAYER ? TWO_IN_A_ROW_SCORE : -TWO_IN_A_ROW_SCORE);
+            }
         }
     }
-    return 0;
+
+    return score;
 }
 
-static int minmax(board_t *board, unsigned depth, bool isMax, int albe[])
-{
-    int score = evaluate(board);
-
-    if (!depth || score)
-        return score;
-    if (!isMovesLeft(board))
-        return 0;
-    int best = isMax ? -1000 : 1000;
+// Minimax function with Alpha-Beta pruning
+int minmax(board_t *board, unsigned depth, bool isMax, int alpha, int beta) {
+    if (depth == 0) {
+        return evaluate(board);
+    }
 
     if (isMax) {
-        for (unsigned i = 0; i < HEIGHT; i++)
-            for (unsigned j = 0; j < WIDTH; j++)
+        int best = INT_MIN;
+        for (unsigned i = 0; i < HEIGHT; i++) {
+            for (unsigned j = 0; j < WIDTH; j++) {
                 if (AT(board) == EMPTY) {
-                    AT(board) = PLAYER;
-                    best = max(best, minmax(board, depth - 1, !isMax, albe));
-                    AT(board) = EMPTY;
-                    albe[0] = max(albe[0], best);
-                    if (albe[1] <= albe[0])
-                        break;
+                    AT(board) = PLAYER; // Make the move
+                    int val = minmax(board, depth - 1, false, alpha, beta);
+                    AT(board) = EMPTY; // Undo the move
+                    best = max(best, val);
+                    alpha = max(alpha, val);
+                    if (beta <= alpha) {
+                        return best; // Prune the remaining branch
+                    }
                 }
+            }
+        }
+        return best;
     } else {
-        for (unsigned i = 0; i < HEIGHT; i++)
-            for (unsigned j = 0; j < WIDTH; j++)
+        int best = INT_MAX;
+        for (unsigned i = 0; i < HEIGHT; i++) {
+            for (unsigned j = 0; j < WIDTH; j++) {
                 if (AT(board) == EMPTY) {
-                    AT(board) = OPPONENT;
-                    best = min(best, minmax(board, depth - 1, !isMax, albe));
-                    AT(board) = EMPTY;
-                    albe[1] = min(albe[1], best);
-                    if (albe[1] <= albe[0])
-                        break;
+                    AT(board) = OPPONENT; // Make the move
+                    int val = minmax(board, depth - 1, true, alpha, beta);
+                    AT(board) = EMPTY; // Undo the move
+                    best = min(best, val);
+                    beta = min(beta, val);
+                    if (beta <= alpha) {
+                        return best; // Prune the remaining branch
+                    }
                 }
+            }
+        }
+        return best;
     }
-    return best;
 }
 
-int main(unsigned ac, const char *av[])
-{
+int main(int ac, const char *av[]) {
     board_t *board = &(board_t) {
         .size = {(unsigned)atoi(av[1]), (unsigned)atoi(av[2])},
         .board = NULL
@@ -141,34 +171,44 @@ int main(unsigned ac, const char *av[])
     av = &av[3];
     for (unsigned i = 0; i < HEIGHT; i++)
         memcpy(&board->board[i * WIDTH], av[i], WIDTH);
-    board->board[WIDTH * HEIGHT] = '\0';
 
     int best_move[2] = {-1, -1};
-    int best_score = -1000;
-    int albe[2] = {-1000, 1000};
+    int best_score = INT_MIN;
+    int alpha = INT_MIN;
+    int beta = INT_MAX;
 
-    for (unsigned i = 0; i < HEIGHT; i++)
+    for (unsigned i = 0; i < HEIGHT; i++) {
         for (unsigned j = 0; j < WIDTH; j++) {
-            if (AT(board) != EMPTY)
-               continue;
-            AT(board) = PLAYER;
-            int score = minmax(board, MAX_DEPTH, false, albe);
-            AT(board) = EMPTY;
-            if (score > best_score) {
-                best_score = score;
-                best_move[0] = (int)i;
-                best_move[1] = (int)j;
-            }
-        }
-
-    if (best_move[0] == -1 && best_move[1] == -1)
-        for (unsigned i = 0; i < HEIGHT; i++)
-            for (unsigned j = 0; j < WIDTH; j++)
-                if (AT(board) == EMPTY) {
+            if (AT(board) == EMPTY) {
+                AT(board) = PLAYER; // AI makes a move
+                int score = minmax(board, MAX_DEPTH, false, alpha, beta);
+                AT(board) = EMPTY; // Undo the move
+                if (score > best_score) {
+                    best_score = score;
                     best_move[0] = (int)i;
                     best_move[1] = (int)j;
                 }
+            }
+        }
+    }
+
+    if (best_move[0] == -1 && best_move[1] == -1) {
+        for (unsigned i = 0; i < HEIGHT; i++) {
+            for (unsigned j = 0; j < WIDTH; j++) {
+                if (AT(board) == EMPTY) {
+                    best_move[0] = (int)i;
+                    best_move[1] = (int)j;
+                    break;
+                }
+            }
+            if (best_move[0] != -1) {
+                break;
+            }
+        }
+    }
 
     printf("%d,%d\n", best_move[0], best_move[1]);
+
+    free(board->board);
     return 0;
 }
